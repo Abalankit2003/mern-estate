8bead8bcc54f7a3b86c2ffed5efd003867c8d417
Ankit Raj <ankitraj90747@gmail.com>
2024-07-06 20:31:17 +0530

    added all browser routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
-
+import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
+import About from './pages/About';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <header />
+      <Header />
       <Routes>
         <Route path = "/" element = {<Home />}></Route>
+        <Route path = "sign-in" element = { <SignIn /> }> </Route>
+        <Route path = "profile" element = {<Profile />}></Route>
+        <Route path = "sign-out" element = { <SignOut />}></Route>
+        <Route path = "about" element = {<About />}></Route>
       </Routes>
     </BrowserRouter>
   )
