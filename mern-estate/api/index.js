2249539ba96706d96446cfddbe9be9e2d72e8fa5
Ankit Raj <ankitraj90747@gmail.com>
2024-07-06 21:27:43 +0530

    connect database

--- mern-estate/api/index.js
+++ mern-estate/api/index.js
@@ -1,4 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const connection = mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("server connection successful"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
